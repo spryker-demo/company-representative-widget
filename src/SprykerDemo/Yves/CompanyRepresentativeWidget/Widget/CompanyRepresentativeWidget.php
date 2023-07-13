fb9b14cb37035c52f39b441f5c5c2d225cb072cf
mabdullah <mohamed.abdullah@spryker.com>
2023-07-13 12:42:37 +0300

    Rename module

---
+++ src/SprykerDemo/Yves/CompanyRepresentativeWidget/Widget/CompanyRepresentativeWidget.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerDemo\Yves\CompanyRepresentativeWidget\Widget;
+
+use Generated\Shared\Transfer\CompanyRepresentativesFilterTransfer;
+use Spryker\Yves\Kernel\Dependency\Widget\WidgetInterface;
+use Spryker\Yves\Kernel\Widget\AbstractWidget;
+
+/**
+ * @method \SprykerDemo\Yves\CompanyRepresentativeWidget\CompanyRepresentativeWidgetFactory getFactory()
+ */
+class CompanyRepresentativeWidget extends AbstractWidget implements WidgetInterface
+{
+    public function __construct()
+    {
+        $this->addCompanyRepresentativesParameter();
+    }
+
+    /**
+     * @return string
+     */
+    public static function getName(): string
+    {
+        return 'CompanyRepresentativeWidget';
+    }
+
+    /**
+     * @return string
+     */
+    public static function getTemplate(): string
+    {
+        return '@CompanyRepresentativeWidget/views/company-representative-widget/company-representative-widget.twig';
+    }
+
+    /**
+     * @return void
+     */
+    protected function addCompanyRepresentativesParameter(): void
+    {
+        $companyId = $this->getFactory()->getCustomerClient()->getCustomer()->getCompanyUserTransfer()->getCompany()->getIdCompany();
+        $customerRepresentativesFilterTransfer = (new CompanyRepresentativesFilterTransfer())->setCompanyId($companyId);
+        $customerRepresentatives = $this->getFactory()->getCompanyRepresentativeClient()->findCompanyRepresentatives($customerRepresentativesFilterTransfer);
+        $this->addParameter('representatives', $customerRepresentatives);
+    }
+}
